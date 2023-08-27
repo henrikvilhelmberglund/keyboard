50b744d4f301c8e11486284d24f43f95a0f85519
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-08-27 21:57:45 +0200

    feat: :sparkles: add mobile support

--- src/routes/Keyboard.svelte
+++ src/routes/Keyboard.svelte
@@ -1,7 +1,7 @@
 <script>
 	import { getMidiNotes, keyboardNotes } from "$lib/midinotes";
 	// import { WebMidi } from "../../node_modules/webmidi/dist/esm/webmidi.esm.min.js";
-	import { Soundfont } from "smplr";
+	import { Soundfont, getSoundfontNames } from "smplr";
 	import { afterUpdate } from "svelte";
 
 	// WebMidi.enable()
@@ -15,48 +15,90 @@
 	// 	.catch((err) => alert(err));
 
 	afterUpdate(() => {
-		document.getElementById("C2").focus();
+		document.getElementById("C3").focus();
 	});
 
 	function setKeyDown(key, bool) {
 		keyDown[key] = bool;
 	}
 
+	function isMobileDevice() {
+		return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+			navigator.userAgent
+		);
+	}
+
+	let limit;
 	let output;
 	// let channel;
 	let mouseDown = false;
 	let keyDown = {};
+	let touching = false;
+	let instrument = "marimba";
+	let instrumentValue = 0;
 	const context = new AudioContext();
-	let channel = new Soundfont(context, { instrument: "marimba" });
+	$: channel = new Soundfont(context, { instrument });
+
+	if (isMobileDevice()) {
+		limit = 61;
+	} else {
+		limit = 73;
+	}
 </script>
 
-<div id="keyboard">
+<input
+	type="range"
+	name=""
+	id=""
+	class="w-[calc(100vw-60px)]"
+	min="0"
+	max="127"
+	bind:value={instrumentValue}
+	on:change={() => {
+		instrument = getSoundfontNames()[instrumentValue];
+		console.log(instrument);
+	}} />
+<div id="keyboard" class="!m-px">
 	{#each getMidiNotes() as note, i}
-		{#if i > 23 && i < 61}
+		{#if i > 35 && i < limit}
 			<button
 				id={note.name}
+				on:touchstart={() => {
+					touching = true;
+					channel.start(note.name);
+					setKeyDown(note.name, true);
+				}}
+				on:touchend={() => {
+					// touching = false;
+					// channel.stop(note.name);
+					setKeyDown(note.name, false);
+				}}
 				on:mousedown={() => {
+					if (touching) return;
 					channel.start(note.name);
 					mouseDown = true;
 					setKeyDown(note.name, true);
 				}}
-				on:mouseleave={() => {
+				on:mouseup={() => {
+					if (touching) return;
 					channel.stop(note.name);
+					mouseDown = false;
 					setKeyDown(note.name, false);
 				}}
 				on:mouseenter={() => {
+					if (touching) return;
 					if (mouseDown) {
 						channel.start(note.name);
 						setKeyDown(note.name, true);
 					}
 				}}
-				on:mouseup={() => {
+				on:mouseleave={() => {
+					if (touching) return;
 					channel.stop(note.name);
-					mouseDown = false;
 					setKeyDown(note.name, false);
 				}}
 				on:keydown={(e) => {
-					console.log(e);
+					// console.log(e);
 					if (!keyDown[keyboardNotes[e.code]]) channel.start(keyboardNotes[e.code]);
 					setKeyDown(keyboardNotes[e.code], true);
 				}}
@@ -66,13 +108,16 @@
 				}}
 				class:black={note.name.includes("#")}
 				class:!bg-primary-300={keyDown[note.name]}
-				class="border-1 h-52 w-11 rounded border-black bg-white" />
+				class="border-1 h-64 w-12 rounded border-black bg-white" />
 		{/if}
 	{/each}
 </div>
 
 <style>
 	.black {
-		@apply absolute h-36 w-6 -translate-x-3 bg-black;
+		@apply absolute h-40 w-8 -translate-x-4 bg-black;
+	}
+	#keyboard button {
+		-webkit-tap-highlight-color: transparent;
 	}
 </style>
