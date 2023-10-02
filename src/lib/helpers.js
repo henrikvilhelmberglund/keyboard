b208cba2456b640909ae482ba83926d32939033c
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-10-02 19:16:32 +0200

    fix: :bug: stop should use note.value

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,24 +1,4 @@
-export function handleTouchStart({ channel, note, e }) {
-	e.preventDefault();
-	if (e.touches.length > 1) {
-	}
-	channel.start(note.name);
-	let touching = true;
-	let keyIsDown = true;
-	return [touching, keyIsDown];
-}
-
-export function handleTouchEnd({ channel, note }) {
-  channel.stop({ note: note.name });
-	let touching = false;
-	let keyIsDown = false;
-	return [touching, keyIsDown];
-}
-
-export function handleMouseDown({ touching, channel, note, e, velocity }) {
-	if (touching) return;
-	console.log(e);
-	// TODO add velocity variable from mouse position (low = high velocity) and use it in mousedown and mouseenter
+function getVelocity(e) {
 	const keyRect = e.target.getBoundingClientRect();
 
 	// Calculate the velocity based on the click position within the key
@@ -27,11 +7,33 @@ export function handleMouseDown({ touching, channel, note, e, velocity }) {
 
 	// Normalize the velocity to a range (e.g., 0 to 1)
 	let factor = 0 - (clickY / keyHeight) * -1;
-  velocity = Math.round(factor * 127);
-  
+	let velocity = Math.round(factor * 127);
+	return velocity;
+}
 
-	console.log(velocity);
-	channel.start({ note: note.name, velocity });
+export function handleTouchStart({ channel, note, e }) {
+	e.preventDefault();
+	if (e.touches.length > 1) {
+	}
+	channel.start(note.value);
+	let touching = true;
+	let keyIsDown = true;
+	return [touching, keyIsDown];
+}
+
+export function handleTouchEnd({ channel, note }) {
+	channel.stop(note.value);
+	let touching = false;
+	let keyIsDown = false;
+	return [touching, keyIsDown];
+}
+
+export function handleMouseDown({ touching, channel, note, e, velocity }) {
+	if (touching) return;
+	console.log(e);
+	velocity = getVelocity(e);
+
+	channel.start({ note: note.value, velocity });
 	let mouseDown = true;
 	let keyIsDown = true;
 	return [mouseDown, keyIsDown, velocity];
@@ -39,25 +41,26 @@ export function handleMouseDown({ touching, channel, note, e, velocity }) {
 
 export function handleMouseUp({ touching, channel, note }) {
 	if (touching) return;
-  channel.stop({ note: note.name });
+	channel.stop(note.value);
 	let mouseDown = false;
 	let keyIsDown = false;
 	return [mouseDown, keyIsDown];
 }
 
-export function handleMouseEnter({ touching, mouseDown, channel, note, velocity }) {
-	let keyIsDown = false;
-	if (touching) return;
+export function handleMouseEnter({ touching, mouseDown, channel, note, e, velocity }) {
+  let keyIsDown = false;
+  if (touching) return;
 	if (mouseDown) {
-		channel.start({ note: note.name, velocity });
+		velocity = getVelocity(e);
+		channel.start({ note: note.value, velocity });
 		keyIsDown = true;
 	}
 	return [keyIsDown];
 }
 
 export function handleMouseLeave({ touching, channel, note }) {
-	if (touching) return;
-  channel.stop({ note: note.name });
+  if (touching) return;
+  channel.stop(note.value);
 	let keyIsDown = false;
 	return [keyIsDown];
 }
