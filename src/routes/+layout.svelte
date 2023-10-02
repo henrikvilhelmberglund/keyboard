5bfe17781be0d563c0e462c2281040241d99fa2b
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-10-02 21:10:22 +0200

    feat: :lipstick: hide stuff in landscape mode, show in portrait mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,27 +15,26 @@
 </script>
 
 <main class="dark:bg-primary-950 bg-primary-100 min-w-screen flex min-h-screen flex-col items-center [&>*]:m-4">
-	<header class="flex">
+	<header class="flex lg:flex landscape:hidden">
 		<ThemeSwitcher />
 		<DarkModeToggle />
 		<!-- <a href="/keyboard" class="btn-primary">Keyboard</a>
 		<a href="/piano" class="btn-primary">Piano</a>
 		<a href="/electricpiano" class="btn-primary">Electric piano</a>
 		<a href="/drums" class="btn-primary">Drums</a> -->
-		<div class="flex gap-40">
+		<div class="absolute top-[50vw] flex flex-col gap-24 md:flex-row md:gap-40">
 			{#each Object.entries(routes) as [url, name]}
 				<a href={url} class="group relative">
 					<span class="relative">
-           
-						<span 
-            class:group-hover:bg-primary-100={url !== currentRoute}
-            class:dark:group-hover:bg-primary-400={url !== currentRoute}
-            class:!bg-primary-200={url === currentRoute || url === "/keyboard" && currentRoute === "/"}
-            class:!dark:bg-primary-500={url === currentRoute || url === "/keyboard" && currentRoute === "/"}
-            class:!mt-2={url === currentRoute || url === "/keyboard" && currentRoute === "/"}
-            class="border-primary-500 bg-primary-300 absolute z-10 w-36 rounded-2xl border-2 p-1 py-4 text-center transition-all ">{name}</span>
+						<span
+							class:group-hover:bg-primary-100={url !== currentRoute}
+							class:dark:group-hover:bg-primary-400={url !== currentRoute}
+							class:!bg-primary-200={url === currentRoute || (url === "/keyboard" && currentRoute === "/")}
+							class:!dark:bg-primary-500={url === currentRoute || (url === "/keyboard" && currentRoute === "/")}
+							class:!mt-2={url === currentRoute || (url === "/keyboard" && currentRoute === "/")}
+							class="border-primary-500 bg-primary-300 absolute z-10 w-36 rounded-2xl border-2 p-1 py-4 text-center transition-all">{name}</span>
 					</span>
-					<span class="border-primary-400 bg-primary-400 absolute inset-0 top-4 w-36 rounded-2xl border-2 p-1 py-6"  />
+					<span class="border-primary-400 bg-primary-400 absolute inset-0 top-4 w-36 rounded-2xl border-2 p-1 py-6" />
 				</a>
 			{/each}
 		</div>
