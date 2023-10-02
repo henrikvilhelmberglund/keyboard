44eeadef02a6aa88b86712757d78841b2b5d5481
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-10-02 18:36:21 +0200

    feat: :sparkles: add mkcert and https for caching

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,17 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import UnoCSS from "@unocss/svelte-scoped/vite";
+import mkcert from "vite-plugin-mkcert";
 
 // silly import to make changes in uno.config.js restart the Vite dev server
 import { reloadMe } from "./uno.config";
 import { defineConfig } from "vite";
 export default defineConfig({
-	server: { host: true },
+	server: { host: true, https: true, proxy: {} },
 	plugins: [
 		UnoCSS({
 			injectReset: "@unocss/reset/tailwind.css",
 		}),
 		sveltekit(),
+		mkcert(),
 	],
 });
