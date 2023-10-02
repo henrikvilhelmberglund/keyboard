dc07c961ad5fc4c071aa033a22ce120536086808
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-10-02 17:22:53 +0200

    feat: :sparkles: add cute buttons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,39 @@
 <script>
+	import { page } from "$app/stores";
 	import DarkModeToggle from "$lib/theme/DarkModeToggle.svelte";
 	import ThemeSwitcher from "$lib/theme/ThemeSwitcher.svelte";
 	import "@unocss/reset/tailwind.css";
+
+	let routes = {
+		"/keyboard": "Keyboard",
+		"/piano": "Piano",
+		"/electricpiano": "Electric Piano",
+		"/drums": "Drums",
+	};
+
+	$: currentRoute = $page.route.id;
 </script>
 
-<main
-	class="dark:bg-primary-950 bg-primary-100 min-w-screen flex min-h-screen flex-col items-center [&>*]:m-4">
-	<header>
+<main class="dark:bg-primary-950 bg-primary-100 min-w-screen flex min-h-screen flex-col items-center [&>*]:m-4">
+	<header class="flex">
 		<ThemeSwitcher />
 		<DarkModeToggle />
-		<a href="/keyboard" class="btn-primary">Keyboard</a>
+		<!-- <a href="/keyboard" class="btn-primary">Keyboard</a>
 		<a href="/piano" class="btn-primary">Piano</a>
 		<a href="/electricpiano" class="btn-primary">Electric piano</a>
-		<a href="/drums" class="btn-primary">Drums</a>
+		<a href="/drums" class="btn-primary">Drums</a> -->
+		<div class="flex gap-40">
+			{#each Object.entries(routes) as [url, name]}
+				<a href={url} class="group relative">
+					<span class="relative">
+						<span 
+            class:!bg-primary-200={url === currentRoute} 
+            class="border-primary-500 bg-primary-300 absolute z-10 w-36 rounded-2xl border-2 p-1 py-4 text-center transition-all group-active:translate-y-2">{name}</span>
+					</span>
+					<span class="border-primary-400 bg-primary-400 absolute inset-0 top-4 w-36 rounded-2xl border-2 p-1 py-6" class:!py-4={url === currentRoute} />
+				</a>
+			{/each}
+		</div>
 	</header>
 
 	<slot />
