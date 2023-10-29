eeb50e12046e3c1719b037c1ce9bc304644be2c3
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-10-29 16:51:52 +0100

    feat: create route for making a splash image

---
+++ src/routes/splash/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+	import Keyboard from "../Keyboard.svelte";
+</script>
+
+<h1 class="text-8xl bg-gradient-to-b from-orange-100 drop-shadow drop-shadow-color-black/50 to-blue-400 bg-clip-text text-transparent">Keyboard</h1>
+<Keyboard />
