0d40f287cb5e84bdb33e3f96f56b009d9f124f00
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2024-10-06 19:30:30 +0200

    build: replace localstorage store with persisted store

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,5 +1,5 @@
 import { writable } from "svelte/store";
-import { persisted } from "svelte-local-storage-store";
+import { persisted } from "svelte-persisted-store";
 
 export const color = persisted(
 	"color",
