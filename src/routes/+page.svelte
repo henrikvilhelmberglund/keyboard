427b147045be5068f2d4c8c375ccd1ffa0c28188
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2024-10-06 19:25:37 +0200

    fix: remove melt stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,6 @@
 	import ThemeSwitcher from "$lib/theme/ThemeSwitcher.svelte";
 	import { browser } from "$app/environment";
 	import { color } from "$lib/stores";
-	import MeltAccordion from "$lib/melt-examples/MeltAccordion.svelte";
 	import Keyboard from "./Keyboard.svelte";
 	import Drums from "./Drums.svelte";
 	import Piano from "./Piano.svelte";
--- svelte.config.js
+++ svelte.config.js
@@ -4,14 +4,13 @@ import adapter from "@sveltejs/adapter-auto";
 // import { vitePreprocess } from "@sveltejs/kit/vite";
 // import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
-import { preprocessMeltUI } from "@melt-ui/pp";
 import sequence from "svelte-sequential-preprocessor";
 
 const dev = process.argv.includes("dev");
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
+	preprocess: sequence([vitePreprocess()]),
 	kit: {
 		adapter: adapter(),
 	},
