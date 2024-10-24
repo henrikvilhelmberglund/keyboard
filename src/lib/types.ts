bda4ad916c8a5c6eb1723af08da37f8af37ed89c
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2024-10-24 17:35:42 +0200

    feat: types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,9 +1,15 @@
 export type Note = {
   name: string;
-  value: number;
+  value: number | string;
 }
 
 import type { DrumMachine, ElectricPiano, Mellotron, Soundfont, SplendidGrandPiano } from "smplr";
 
 export type ValidInstruments = Soundfont | ElectricPiano | Mellotron | DrumMachine | SplendidGrandPiano;
 
+export type InstrumentType = {
+  startingInstrument: string;
+  instrumentList: string[];
+  instrumentNotes: Note[];
+  instrumentType: ValidInstruments;
+}
