bfff1708f01aeacd7f1319b28d34a1a2ebc332b0
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2024-10-30 17:13:27 +0100

    fix: fix keyboard import for /splash route

--- src/routes/splash/+page.svelte
+++ src/routes/splash/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	import Keyboard from "../Keyboard.svelte";
+	import Keyboard from "$lib/instruments/Keyboard.svelte";
 </script>
 
 <h1 class="text-8xl bg-gradient-to-b from-orange-100 drop-shadow drop-shadow-color-black/50 to-blue-400 bg-clip-text text-transparent">Keyboard</h1>
